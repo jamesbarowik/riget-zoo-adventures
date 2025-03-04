7e49fc40a518621afb2999f56bd9d60b5a7717fa
James <barowikajames@gmail.com>
2025-03-04 12:32:38 +0000

    fix: folder structure

--- V2/database/rza.sql
+++ V2/database/rza.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Feb 04, 2025 at 11:39 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Mar 04, 2025 at 12:32 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `rza`
 --
-CREATE DATABASE IF NOT EXISTS `rza` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `rza` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `rza`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int NOT NULL,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `group` text NOT NULL,
+  `sign_up_date` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `first_name`, `last_name`, `email`, `password`, `group`, `sign_up_date`) VALUES
+(4, 'super', 'admin', 'su@rzl.com', '$2y$10$.ZDCBQVW9HwbQtJt4hTEreXHxHosY6rpZbeZf9xTuT.9N9R4MqpZ6', 'ROOT', 1740648280);
 
 -- --------------------------------------------------------
 
@@ -30,11 +53,11 @@ USE `rza`;
 --
 
 CREATE TABLE `hotel_booking` (
-  `hotel_booking_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created` int(11) NOT NULL,
-  `nights` int(11) NOT NULL,
-  `hotel_room_id` int(11) NOT NULL
+  `hotel_booking_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `created` int NOT NULL,
+  `nights` int NOT NULL,
+  `hotel_room_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -44,12 +67,19 @@ CREATE TABLE `hotel_booking` (
 --
 
 CREATE TABLE `hotel_rooms` (
-  `room_id` int(11) NOT NULL,
-  `type` text NOT NULL,
-  `occupancy` int(11) NOT NULL,
-  `no_of_rooms` int(11) NOT NULL,
+  `room_id` int NOT NULL,
+  `type` text COLLATE utf8mb4_general_ci NOT NULL,
+  `occupancy` int NOT NULL,
+  `no_of_rooms` int NOT NULL,
   `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `hotel_rooms`
+--
+
+INSERT INTO `hotel_rooms` (`room_id`, `type`, `occupancy`, `no_of_rooms`, `price`) VALUES
+(1, 'Family', 4, 10, 220);
 
 -- --------------------------------------------------------
 
@@ -58,10 +88,10 @@ CREATE TABLE `hotel_rooms` (
 --
 
 CREATE TABLE `loyalty` (
-  `loyalty_id` int(11) NOT NULL,
-  `points` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `date` int(11) NOT NULL
+  `loyalty_id` int NOT NULL,
+  `points` int NOT NULL,
+  `user_id` int NOT NULL,
+  `date` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -71,10 +101,10 @@ CREATE TABLE `loyalty` (
 --
 
 CREATE TABLE `staying_in` (
-  `staying_id` int(11) NOT NULL,
-  `booking_id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `no_of_people` int(11) NOT NULL
+  `staying_id` int NOT NULL,
+  `booking_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `no_of_people` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -84,11 +114,20 @@ CREATE TABLE `staying_in` (
 --
 
 CREATE TABLE `tickets` (
-  `ticket_id` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
+  `ticket_id` int NOT NULL,
+  `type` text COLLATE utf8mb4_general_ci NOT NULL,
   `price` float NOT NULL,
-  `no_of_tickets` int(11) NOT NULL
+  `no_of_tickets` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`ticket_id`, `type`, `price`, `no_of_tickets`) VALUES
+(1, 'Child', 6, 120),
+(2, 'Adult', 27, 120),
+(3, 'Old Age Person', 12, 120);
 
 -- --------------------------------------------------------
 
@@ -97,10 +136,10 @@ CREATE TABLE `tickets` (
 --
 
 CREATE TABLE `ticket_booking` (
-  `ticket_booking_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created` int(11) NOT NULL,
-  `arrival` int(11) NOT NULL
+  `ticket_booking_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `created` int NOT NULL,
+  `arrival` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -110,12 +149,20 @@ CREATE TABLE `ticket_booking` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `email` text NOT NULL,
-  `password` text NOT NULL,
-  `sign_up_date` int(11) NOT NULL,
-  `user_type_id` int(11) NOT NULL
+  `user_id` int NOT NULL,
+  `email` text COLLATE utf8mb4_general_ci NOT NULL,
+  `password` text COLLATE utf8mb4_general_ci NOT NULL,
+  `sign_up_date` int NOT NULL,
+  `user_type_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `sign_up_date`, `user_type_id`) VALUES
+(2, 'test@test.c', '$2y$10$ECcznOGgflsTzHhUNwdpYuCNZlQ5nRPTHquWT623rJNnMnwdeuToW', 1740649370, 2),
+(3, 'test@test.c', '$2y$10$FwxtzTqAfWasoDIzpeDuGOBYanW.pnvditNl00h1zB9HoS/GdqzwG', 1740649407, 2);
 
 -- --------------------------------------------------------
 
@@ -124,10 +171,17 @@ CREATE TABLE `users` (
 --
 
 CREATE TABLE `user_type` (
-  `user_type_id` int(11) NOT NULL,
-  `group` text NOT NULL,
+  `user_type_id` int NOT NULL,
+  `group` text COLLATE utf8mb4_general_ci NOT NULL,
   `discount` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`user_type_id`, `group`, `discount`) VALUES
+(2, 'High School', 25);
 
 -- --------------------------------------------------------
 
@@ -136,15 +190,21 @@ CREATE TABLE `user_type` (
 --
 
 CREATE TABLE `visits` (
-  `visit_id` int(11) NOT NULL,
-  `ticket_booking_id` int(11) NOT NULL,
-  `ticket_id` int(11) NOT NULL,
-  `no_of_tickets` int(11) NOT NULL
+  `visit_id` int NOT NULL,
+  `ticket_booking_id` int NOT NULL,
+  `ticket_id` int NOT NULL,
+  `no_of_tickets` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `hotel_booking`
@@ -213,62 +273,74 @@ ALTER TABLE `visits`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `hotel_booking`
 --
 ALTER TABLE `hotel_booking`
-  MODIFY `hotel_booking_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `hotel_booking_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `hotel_rooms`
 --
 ALTER TABLE `hotel_rooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `room_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `loyalty`
 --
 ALTER TABLE `loyalty`
-  MODIFY `loyalty_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `loyalty_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `staying_in`
 --
 ALTER TABLE `staying_in`
-  MODIFY `staying_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `staying_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ticket_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `ticket_booking`
 --
 ALTER TABLE `ticket_booking`
-  MODIFY `ticket_booking_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ticket_booking_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_type`
 --
 ALTER TABLE `user_type`
-  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_type_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `visits`
 --
 ALTER TABLE `visits`
-  MODIFY `visit_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `visit_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `hotel_booking`
+--
+ALTER TABLE `hotel_booking`
+  ADD CONSTRAINT `hotel_booking_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `loyalty`
@@ -294,13 +366,7 @@ ALTER TABLE `ticket_booking`
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `hotel_booking` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `user_type`
---
-ALTER TABLE `user_type`
-  ADD CONSTRAINT `user_type_ibfk_1` FOREIGN KEY (`user_type_id`) REFERENCES `users` (`user_type_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`user_type_id`) REFERENCES `user_type` (`user_type_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `visits`
